2e811e903f7f3b6e45ea27d2a9e1a688e903be37
simon <simoonf@vip.qq.com>
2025-01-02 23:12:16 +0800

    feat: 添加 tsup 配置文件，更新入口文件和 package.json 配置

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-// index
+import path from 'path';
+
+export function getDir() {
+  return __dirname;
+}
+
+export function resolvePath(relativePath: string) {
+  return path.resolve(__dirname, relativePath);
+}
