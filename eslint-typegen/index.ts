7ed39f7d94c1240049d56a720e2e5b0d021aabc8
simon <simoonf@vip.qq.com>
2025-01-13 19:35:54 +0800

    feat: add ESLint type generation script and remove old implementation

---
+++ eslint-typegen/index.ts
@@ -0,0 +1,45 @@
+import fs from 'node:fs/promises';
+import { mergeFlatConfigs, stylistic, typescript } from '../src';
+import { builtinRules } from 'eslint/use-at-your-own-risk';
+import { flatConfigsToRulesDTS } from 'eslint-typegen/core';
+import { javascript } from 'src/configs/javascript';
+
+async function generateEslintType() {
+  const configs = await mergeFlatConfigs(
+    {
+      plugins: {
+        '': {
+          rules: Object.fromEntries(builtinRules.entries()),
+        },
+      },
+    },
+    javascript(),
+    typescript(),
+    stylistic(),
+  );
+
+  const configNames = configs.map(i => i.name).filter(Boolean) as string[];
+
+  let types = await flatConfigsToRulesDTS(configs, {
+    includeAugmentation: false,
+  });
+
+  types += `
+// Names of all the configs
+export type ConfigNames = ${configNames.map(i => `'${i}'`).join(' | ')}
+`;
+
+  try {
+    await fs.writeFile('src/eslintype.d.ts', types);
+
+    // eslint-disable-next-line no-console
+    console.info('ESLint type definitions generated successfully.');
+  }
+  catch (error) {
+    console.error('Failed to write ESLint type definitions:', error);
+  }
+}
+
+generateEslintType().catch(error => {
+  console.error('Error generating ESLint type definitions:', error);
+});
